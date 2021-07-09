49464ab97b55cacdb1d04a80cc6543ab8554e131
Senimtra <senimtra@hotmail.com>
2021-07-09 15:35:11 +0200

    create game class

--- game.js
+++ game.js
@@ -2,3 +2,11 @@
 ######################################
 ## JS Canvas Game - Quadrant Patrol ##
 ###################################### */
+
+class Game {
+   constructor(canvas) {
+      this.canvas = canvas;
+      // ### access the drawing context ###
+      this.context = canvas.getContext('2d');
+   }
+}
--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 ######################################
 ## JS Canvas Game - Quadrant Patrol ##
 ###################################### */
+
+// ### retrieve the canvas node ###
+const canvasElement = document.querySelector('canvas');
+
+// ### create new game instance ###
+const game = new Game(canvasElement);
+
